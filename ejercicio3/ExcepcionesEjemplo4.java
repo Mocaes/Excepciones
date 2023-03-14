5c573aa0f08d5d706afb7b023f6f5c4dafceeafe
Mocaes <118849292+Mocaes@users.noreply.github.com>
2023-03-14 15:22:16 +0100

    Add files via upload

---
+++ ejercicio3/ExcepcionesEjemplo4.java
@@ -0,0 +1,19 @@
+package ejercicio3;
+
+public class ExcepcionesEjemplo4 {
+	public static void main(String[] args) {
+	try {
+		int resultado = dividir(10, 0);
+	 	System.out.println("El resultado es: " + resultado);
+	} catch (ArithmeticException e) {
+		System.out.println("Error: división entre cero.");
+	}
+	}
+	public static int dividir(int a, int b) throws ArithmeticException {
+		if (b == 0) {
+		throw new ArithmeticException();
+	}
+		return a / b;
+	}
+}
+ 
